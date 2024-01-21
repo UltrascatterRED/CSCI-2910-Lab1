e2427058351eb9f002e7d096a3872202158b18be
UltrascatterRED <72528560+UltrascatterRED@users.noreply.github.com>
2024-01-21 16:14:17 -0500

    Added .cs files and screenshots

---
+++ Program.cs
@@ -0,0 +1,184 @@
+﻿namespace bartonn_csci2910_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<VideoGame> games = new List<VideoGame>();
+            string currentLine;
+            try
+            {
+                Console.WriteLine("Application Loading (may take a moment)...");
+                //videogames.csv MUST be in the same directory as Program.cs to be read.
+                StreamReader sr = new StreamReader("..\\..\\..\\videogames.csv");
+                //skips header record in videogames.csv
+                sr.ReadLine();
+                currentLine = sr.ReadLine();
+
+                while (currentLine != null)
+                {
+                    //stores current record as VideoGame object
+                    string[] currentFields = currentLine.Split(',');
+
+                    string currentName = currentFields[0];
+                    string currentPlatform = currentFields[1];
+                    int currentYear = int.Parse(currentFields[2]);
+                    string currentGenre = currentFields[3];
+                    string currentPublisher = currentFields[4];
+                    decimal currentNA_Sales = decimal.Parse(currentFields[5]);
+                    decimal currentEU_Sales = decimal.Parse(currentFields[6]);
+                    decimal currentJP_Sales = decimal.Parse(currentFields[7]);
+                    decimal currentOther_Sales = decimal.Parse(currentFields[8]);
+                    decimal currentGlobal_Sales = decimal.Parse(currentFields[9]);
+
+                    games.Add
+                    (
+                        new VideoGame
+                        (
+                            currentName, currentPlatform, currentYear, currentGenre, currentPublisher, currentNA_Sales,
+                            currentEU_Sales, currentJP_Sales, currentOther_Sales, currentGlobal_Sales
+                        )
+                    );
+                    //reads in next line
+                    currentLine = sr.ReadLine();
+                }
+
+                SortVideoGameList(games);
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the Video Game Database");
+                    Console.WriteLine("-----------------------------------------------------------------");
+                    Console.WriteLine("1.) Search by Publisher");
+                    Console.WriteLine("2.) Search by Genre");
+                    Console.WriteLine("3.) Exit");
+                    Console.WriteLine();
+                    Console.Write("Number of your Selection (1,2,3): ");
+                    try
+                    {
+                        switch (int.Parse(Console.ReadLine()))
+                        {
+                            case 1:
+                                //******************************************************************************
+                                //extracts all records published by user-inputted publisher
+                                Console.Write("Enter publisher to search for:\t");
+                                PublisherData(games, Console.ReadLine());
+                                Console.WriteLine("Press [ENTER] to continue");
+                                Console.ReadLine();
+                                continue;
+                            case 2:
+                                //******************************************************************************
+                                //extracts all records of user-inputted genre
+                                Console.Write("Enter genre to search for:\t");
+                                GenreData(games, Console.ReadLine());
+                                Console.WriteLine("Press [ENTER] to continue");
+                                Console.ReadLine();
+                                continue;
+                            case 3:
+                                break;
+                            default:
+                                continue;
+                        }
+                    }
+                    catch(Exception ex) 
+                    {
+                        continue;
+                    }
+                    break;
+                }
+                
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        //this method was obtained from a StackOverflow forum:
+        //(https://stackoverflow.com/questions/2094239/swap-two-items-in-listt)
+        public static void Swap<T>(IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+        public static void DisplayVideoGameList(List<VideoGame> gameList)
+        {
+            //displays all records (including record number) to console
+            int recordNum = 1;
+            foreach (VideoGame game in gameList)
+            {
+                Console.WriteLine(recordNum + "\t" + game.ToString());
+                recordNum++;
+            }
+        }
+        public static void SortVideoGameList(List<VideoGame> gameList)
+        {
+            //selection sort
+            int unsortedLength = gameList.Count;
+            while (unsortedLength > 1)
+            {
+                int startingIndex = gameList.Count - unsortedLength;
+                //for loop iterates to value of 'games.Count' due to
+                //previous skipping of file's header record
+                for (int i = startingIndex + 1; i < gameList.Count; i++)
+                {
+                    if (gameList.ElementAt(i).CompareTo(gameList.ElementAt(startingIndex)) < 0)
+                    {
+                        Swap(gameList, startingIndex, i);
+                    }
+                }
+                unsortedLength--;
+            }
+        }
+        public static void PublisherData(List<VideoGame> baseGameList, string publisher)
+        {
+            List<VideoGame> publisherGames = new List<VideoGame>();
+            foreach (VideoGame game in baseGameList)
+            {
+                //toUpper() used as a precaution against case inconsistency
+                if (game.GetPublisher().ToUpper() == publisher.ToUpper())
+                {
+                    publisherGames.Add(game);
+                }
+            }
+            if (publisherGames.Count == 0)
+            {
+                Console.WriteLine("Publisher '" + publisher + "' not found.");
+                return;
+            }
+            SortVideoGameList(publisherGames);
+            DisplayVideoGameList(publisherGames);
+
+            decimal publisherPercentage = Math.Round((decimal)publisherGames.Count / baseGameList.Count * 100, 2);
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine(publisherGames.Count + " out of " + baseGameList.Count + " games were developed by " +
+                publisher + " (" + publisherPercentage + "%)\n");
+        }
+        public static void GenreData(List<VideoGame> baseGameList, string genre)
+        {
+            List<VideoGame> genreGames = new List<VideoGame>();
+            foreach (VideoGame game in baseGameList)
+            {
+                //toUpper() used as a precaution against case inconsistency
+                if (game.GetGenre().ToUpper() == genre.ToUpper())
+                {
+                    genreGames.Add(game);
+                }
+            }
+            if (genreGames.Count == 0)
+            {
+                Console.WriteLine("Genre '" + genre + "' not found.");
+                return;
+            }
+            SortVideoGameList(genreGames);
+            DisplayVideoGameList(genreGames);
+
+            decimal publisherPercentage = Math.Round((decimal)genreGames.Count / baseGameList.Count * 100, 2);
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine(genreGames.Count + " out of " + baseGameList.Count + " games are '" +
+                genre + "' games (" + publisherPercentage + "%)\n");
+        }
+    }
+}
---
+++ VideoGame.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace bartonn_csci2910_lab1
+{
+    internal class VideoGame : IComparable
+    {
+        private string Name;
+        private string Platform;
+        private int Year;
+        private string Genre;
+        private string Publisher;
+        private decimal NA_Sales;
+        private decimal EU_Sales;
+        private decimal JP_Sales;
+        private decimal Other_Sales;
+        private decimal Global_Sales;
+
+        //basic parameterized constructor
+        public VideoGame
+            (
+                string name, string platform, int year, string genre, string publisher, decimal na_Sales, 
+                decimal eu_Sales, decimal jp_Sales, decimal other_Sales, decimal global_Sales
+            )
+        {
+            SetName(name);
+            SetPlatform(platform);
+            SetYear(year);
+            SetGenre(genre);
+            SetPublisher(publisher);
+            SetNA_Sales(na_Sales);
+            SetEU_Sales(eu_Sales);
+            SetJP_Sales(jp_Sales);
+            SetOther_Sales(other_Sales);
+            SetGlobal_Sales(global_Sales);
+        }
+
+        //getters
+        public string GetName() { return Name; }
+        public string GetPlatform() { return Platform; }
+        public int GetYear() { return Year; }
+        public string GetGenre() { return Genre; }
+        public string GetPublisher() { return Publisher; }
+        public decimal GetNA_Sales() { return NA_Sales; }
+        public decimal GetEU_Sales() { return EU_Sales; }
+        public decimal GetJP_Sales() { return JP_Sales; }
+        public decimal GetOther_Sales() { return Other_Sales; }
+        public decimal GetGlobal_Sales() { return Global_Sales; }
+
+        //setters
+        private void SetName( string name ) { Name = name; }
+        private void SetPlatform( string platform ) { Platform = platform; }
+        private void SetYear( int year ) { Year = year; }
+        private void SetGenre(string genre) { Genre = genre; }
+        private void SetPublisher(string publisher) { Publisher = publisher; }
+        private void SetNA_Sales(decimal na_sales) { NA_Sales = na_sales; }
+        private void SetEU_Sales(decimal eu_sales) { EU_Sales = eu_sales; }
+        private void SetJP_Sales(decimal jp_sales) { JP_Sales = jp_sales; }
+        private void SetOther_Sales(decimal other_sales) { Other_Sales = other_sales; }
+        private void SetGlobal_Sales(decimal global_sales) { Global_Sales = global_sales; }
+
+        //methods
+        public int CompareTo(object? obj)
+        {
+            //base code for this method obtained from the Microsoft C# IComparable page
+            //(https://learn.microsoft.com/en-us/dotnet/api/system.icomparable?view=net-8.0)
+
+            if (obj == null) return 1;
+
+            VideoGame otherGame = obj as VideoGame;
+            if (otherGame != null)
+                return this.Name.CompareTo(otherGame.Name);
+            else
+                throw new ArgumentException("Object is not of type 'VideoGame'.");
+        }
+        public override string ToString()
+        {
+            return 
+                Name + " | " + 
+                Platform + " | " + 
+                Year + " | " + 
+                Genre + " | " +
+                Publisher + " | " +
+                NA_Sales + " | " +
+                EU_Sales + " | " +
+                JP_Sales + " | " +
+                Other_Sales + " | " +
+                Global_Sales + "\n";
+        }
+    }
+}
